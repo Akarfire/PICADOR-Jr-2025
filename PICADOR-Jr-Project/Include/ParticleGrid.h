fe72d1cf5f9345c648ae577fab87b6e9cc76fdba
K <Akarfire@gmail.com>
2025-10-09 16:07:37 +0300

    Particle grid changes + transfer implementation

--- PICADOR-Jr-Project/Include/ParticleGrid.h
+++ PICADOR-Jr-Project/Include/ParticleGrid.h
@@ -7,13 +7,17 @@ class ParticleGrid: public Grid
 	// Storage of particles
 	std::vector<std::vector<Particle>> particlesInCells;
 
+	// Converts 2D cell coordinates into a 1D array index
+	size_t recalculateCellIndex(size_t i, size_t j) const;
+
 public:
 	ParticleGrid(size_t resolutionX_, size_t resolutionY_, double deltaX_, double deltaY_, const Vector3& origin_): Grid(resolutionX_, resolutionY_, deltaX_, deltaY_, origin_) {}
 
 	// Returns a reference to the vector of particles in the specified cell
-	const std::vector<Particle>& getParticlesInCell(size_t i, size_t j) const;
+	const std::vector<Particle>& getParticlesInCell(size_t i, size_t j) const { return particlesInCells[recalculateCellIndex(i, j)]; }
 
 	// Hands a particle from one cell over to another
 	// Particle ID is the index of the particle in the owner cell
-	void particleCellTransfer(size_t particleID, size_t ownerCell_i, size_t ownerCell_j, size_t receiverCell_i, size_t receiverCell_j);
+	// Returns 0 if successful, 1 - if failed
+	int particleCellTransfer(size_t particleID, size_t ownerCell_i, size_t ownerCell_j, size_t receiverCell_i, size_t receiverCell_j);
 };
