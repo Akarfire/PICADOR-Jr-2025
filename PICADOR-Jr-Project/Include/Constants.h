d7048e49fe5f71704b405350b6f32b948282cf11
K <Akarfire@gmail.com>
2025-10-23 13:56:38 +0300

    Constants code cosmetic change

--- PICADOR-Jr-Project/Include/Constants.h
+++ PICADOR-Jr-Project/Include/Constants.h
@@ -10,5 +10,4 @@ public:
     inline static const double SpeedOfLight = 29979245800;
     inline static const double ElectronMass = 9.10938215e-28;
     inline static const double ElectronCharge = -4.80320427e-10;
-
 };
