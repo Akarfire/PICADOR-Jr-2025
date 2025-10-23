eb855bbfdab59ac4cb924c7eb23d02863e2664a1
K <Akarfire@gmail.com>
2025-10-23 19:33:22 +0300

    [DEV] Constants build fix

--- PICADOR-Jr-Project/Include/Constants.h
+++ PICADOR-Jr-Project/Include/Constants.h
@@ -5,9 +5,9 @@
 class Constants
 {
 
-Constants() {}
+    Constants() {}
 public:
-    inline static const double SpeedOfLight = 29979245800;
-    inline static const double ElectronMass = 9.10938215e-28;
-    inline static const double ElectronCharge = -4.80320427e-10;
+    static const double SpeedOfLight;
+    static const double ElectronMass;
+    static const double ElectronCharge;
 };
