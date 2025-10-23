48673147188927cfc3270eba36116a7a5e862e37
vassyusha <romanoffmiss@yandex.ru>
2025-10-23 16:52:39 +0300

    the grid filling was changed

--- PICADOR-Jr-Project/Source/FieldGrid.cpp
+++ PICADOR-Jr-Project/Source/FieldGrid.cpp
@@ -3,7 +3,15 @@
 FieldGrid::FieldGrid(size_t resolutionX_, size_t resolutionY_, double deltaX_, double deltaY_, const Vector3& origin_, size_t padding_):
 	Grid(resolutionX_, resolutionY_, deltaX_, deltaY_, origin_, padding_)
 {
-	this->nodeArray = std::vector<FieldData>((this->resolutionX + 2*this->padding) * (this->resolutionY+2*this->padding));
+	this->nodeArray = std::vector<FieldData>((this->resolutionX + 2*this->padding) * (this->resolutionY+2*this->padding), FieldData());
+	for (int i = 0; i < resolutionX; i++) 
+	{
+		for (int j = 0; j < resolutionY; j++)
+		{
+			this->nodeArray[(i + padding) * resolutionX + (j + padding)].B = i;
+			this->nodeArray[(i + padding) * resolutionX + (j + padding)].E = j;
+		}
+	}
 }
 
 // Returns an editable reference to the specified field node
@@ -22,18 +30,18 @@ const FieldData& FieldGrid::getNodeAt(GRID_INDEX i, GRID_INDEX j) const
 // Returns interpolated values of fields in the given point
 FieldData FieldGrid::getFieldsAt(const Vector3& location) const
 {
-	size_t i = this->getCell(location).first, j = this->getCell(location).first;
+	size_t i = this->getCell(location).first, j = this->getCell(location).second;
 	Vector3 cellOrigin(this->origin.x + i * this->deltaX, this->origin.y + j * this->deltaY, 0.0);
 	Vector3 delta(this->deltaX, this->deltaY, 1.0);
 
 	Vector3 newLocation = (location - cellOrigin) / delta;
 
 	FieldData interpolated;
-	interpolated.E = (this->getNodeAt(i, j)).E* (1 - newLocation.x) * (1 - newLocation.y) + (this->getNodeAt(i, j + 1)).E * (1 - newLocation.x) * newLocation.y + 
-		(this->getNodeAt(i + 1, j)).E * newLocation.x * (1 - newLocation.y) + (this->getNodeAt(i + 1, j+1)).E *  newLocation.x * newLocation.y;
+	interpolated.E = (this->getNodeAt(i, j)).E* (1.0 - newLocation.x) * (1.0 - newLocation.y) + (this->getNodeAt(i, j + 1)).E * (1.0 - newLocation.x) * newLocation.y + 
+		(this->getNodeAt(i + 1, j)).E * newLocation.x * (1.0 - newLocation.y) + (this->getNodeAt(i + 1, j+1)).E *  newLocation.x * newLocation.y;
 
-	interpolated.B = (this->getNodeAt(i, j)).B * (1 - newLocation.x) * (1 - newLocation.y) + (this->getNodeAt(i, j + 1)).B * (1 - newLocation.x) * newLocation.y +
-		(this->getNodeAt(i + 1, j)).B * newLocation.x * (1 - newLocation.y) + (this->getNodeAt(i + 1, j + 1)).B * newLocation.x * newLocation.y;
+	interpolated.B = (this->getNodeAt(i, j)).B * (1.0 - newLocation.x) * (1.0 - newLocation.y) + (this->getNodeAt(i, j + 1)).B * (1.0 - newLocation.x) * newLocation.y +
+		(this->getNodeAt(i + 1, j)).B * newLocation.x * (1.0 - newLocation.y) + (this->getNodeAt(i + 1, j + 1)).B * newLocation.x * newLocation.y;
 
 	interpolated.J = (this->getNodeAt(i, j)).J * (1 - newLocation.x) * (1 - newLocation.y) + (this->getNodeAt(i, j + 1)).J * (1 - newLocation.x) * newLocation.y +
 		(this->getNodeAt(i + 1, j)).J * newLocation.x * (1 - newLocation.y) + (this->getNodeAt(i + 1, j + 1)).J * newLocation.x * newLocation.y;
