86ab9c7cf019471140dbf27d70a859efc3c89e97
K <Akarfire@gmail.com>
2025-10-24 21:32:03 +0300

    [TEST] Boris method convergence test sample

--- PICADOR-Jr-Project/samples/BorisConvergenceTest.cpp
+++ PICADOR-Jr-Project/samples/BorisConvergenceTest.cpp
@@ -13,9 +13,14 @@
 
 int main()
 {
+    std::vector<double> deltaTimes;
+    std::vector<double> locationErrors;
+    std::vector<double> velocityErrors;
+
+
     // Initializing static field
 
-    double EZero = 10;
+    double EZero = 1;
 
     FieldData staticFieldData;
     staticFieldData.E = Vector3(EZero, 0, 0);
@@ -24,44 +29,50 @@ int main()
 
     StaticField staticField(staticFieldData);
 
-    // Initializing particle grid
-    ParticleGrid particleGrid(2, 2, 200, 200, Vector3(-100, -100), 0);
+    for (int d = 1; d < 100; d++)
+    {    
+        // Initializing particle grid
+        ParticleGrid particleGrid(2, 2, 2000, 2000, Vector3(-1000, -1000), 0);
 
-    // Adding single particle to the grid
-    Particle testParticle(Constants::ElectronMass, Constants::ElectronCharge, Vector3(0, 0, 0), Vector3::Zero);
-    particleGrid.editParticlesInCell(0, 0).push_back(testParticle);
+        // Adding single particle to the grid
+        Particle testParticle(Constants::ElectronMass, Constants::ElectronCharge, Vector3(0, 0, 0), Vector3::Zero);
+        particleGrid.editParticlesInCell(0, 0).push_back(testParticle);
 
-    size_t NumInterations = 100;
-    double timeStep = abs(Constants::ElectronMass * Constants::SpeedOfLight 
-        / (Constants::ElectronCharge * EZero * NumInterations));
+        size_t NumInterations = 100 * d;
+        double timeStep = abs(Constants::ElectronMass * Constants::SpeedOfLight 
+            / (Constants::ElectronCharge * EZero * NumInterations));
 
-    // Initializing core
-    PicadorJrCore core(&staticField, &particleGrid, timeStep, timeStep * 100);
+        // Initializing core
+        PicadorJrCore core(&staticField, &particleGrid, timeStep, NumInterations);
 
-    ParticleSolver particleSolver(&core);
-    core.insertModule(&particleSolver);
+        ParticleSolver particleSolver(&core);
+        core.insertModule(&particleSolver);
 
-    core.run();
+        core.run();
 
-    // Checking results
-    auto& particles = core.getParticleGrid()->getParticlesInCell(0, 0);
+        // Checking results
+        auto& particles = core.getParticleGrid()->getParticlesInCell(0, 0);
 
-    double finalX = (sqrt(2) - 1) * Constants::ElectronMass * Constants::SpeedOfLight * Constants::SpeedOfLight / (Constants::ElectronCharge * EZero);
-    
-    double finalImpulseX = Constants::ElectronMass * Constants::SpeedOfLight;
-    double finalVelocityX = -1 * finalImpulseX / (Constants::ElectronMass * sqrt(1 + (finalImpulseX * finalImpulseX / ((Constants::ElectronMass * Constants::SpeedOfLight) 
-                    * (Constants::ElectronMass * Constants::SpeedOfLight)))));;
+        double finalX = (sqrt(2) - 1) * Constants::ElectronMass * Constants::SpeedOfLight * Constants::SpeedOfLight / (Constants::ElectronCharge * EZero);
+        
+        double finalImpulseX = Constants::ElectronMass * Constants::SpeedOfLight;
+        double finalVelocityX = -1 * finalImpulseX / (Constants::ElectronMass * sqrt(1 + (finalImpulseX * finalImpulseX / ((Constants::ElectronMass * Constants::SpeedOfLight) 
+                        * (Constants::ElectronMass * Constants::SpeedOfLight)))));;
 
-    double xError = abs(finalX - particles[0].location.x);
-    double vError = abs(finalVelocityX - particles[0].velocity.x);
+        double xError = abs(finalX - particles[0].location.x);
+        double vError = abs(finalVelocityX - particles[0].velocity.x);
+
+        deltaTimes.push_back(timeStep);
+        locationErrors.push_back(xError);
+        velocityErrors.push_back(vError);
+    }
     
     // Writing data into file
     std::ofstream outputFile;
     outputFile.open("./BorisConvergence.txt", std::ios::out);
-
-    outputFile << NumInterations << std::endl;
-    outputFile << xError << std::endl;
-    outputFile << vError << std::endl;
+    
+    for (size_t i = 0; i < deltaTimes.size(); i++)
+        outputFile << deltaTimes[i] << ", " << locationErrors[i] << ", " << velocityErrors[i] << std::endl;
 
     outputFile.close();
 
