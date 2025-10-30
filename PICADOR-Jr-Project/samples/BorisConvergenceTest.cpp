ba27a53abb020ecdc02ea7c149c507113e284847
K <Akarfire@gmail.com>
2025-10-30 21:39:55 +0300

    [TEST] Boris convergence testing

--- PICADOR-Jr-Project/samples/BorisConvergenceTest.cpp
+++ PICADOR-Jr-Project/samples/BorisConvergenceTest.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 #include "PicadorJrCore.h"
 
@@ -11,13 +12,54 @@
 
 #include "Constants.h"
 
+
+// Tests
+void AccelerationTestX(std::vector<double>& outDeltaTimes, std::vector<double>& outLocationErrors, std::vector<double>& outVelocityErrors, size_t iterations);
+void AccelerationTestY(std::vector<double>& outDeltaTimes, std::vector<double>& outLocationErrors, std::vector<double>& outVelocityErrors, size_t iterations);
+void OscilationTest(std::vector<double>& outDeltaTimes, std::vector<double>& outLocationErrors, std::vector<double>& outVelocityErrors, size_t iterations);
+
 int main()
 {
     std::vector<double> deltaTimes;
     std::vector<double> locationErrors;
     std::vector<double> velocityErrors;
 
+    std::cout << "Running" << std::endl;
 
+    size_t maxIterations = 10000;
+    size_t step = 20;
+    size_t scale = 1;
+    std::string fileName = "BorisY.txt";
+
+    for (size_t d = 100; d < maxIterations; d += step)
+    {  
+        //AccelerationTestX(deltaTimes, locationErrors, velocityErrors, d * scale);
+        //AccelerationTestY(deltaTimes, locationErrors, velocityErrors, d * scale);
+        OscilationTest(deltaTimes, locationErrors, velocityErrors, d * scale);
+        std::cout << "Progress: " << pow((double)d / maxIterations, 2) * 100 << "%" << std::endl;
+    }
+
+    std::cout << "Writing data" << std::endl;
+
+    // Writing data into file
+    std::ofstream outputFile;
+    outputFile.open("./" + fileName, std::ios::out);
+    
+    outputFile << "#DeltaTime, Location Error, Velocity Error" << std::endl << "/InverseX" << std::endl;
+
+    for (size_t i = 0; i < deltaTimes.size(); i++)
+        outputFile << deltaTimes[i] << ", " << locationErrors[i] << ", " << velocityErrors[i] << std::endl;
+
+    outputFile.close();
+
+    std::cout << "Complete" << std::endl;
+
+    return 0;
+}
+
+
+void AccelerationTestX(std::vector<double>& outDeltaTimes, std::vector<double>& outLocationErrors, std::vector<double>& outVelocityErrors, size_t iterations)
+{
     // Initializing static field
 
     double EZero = 1;
@@ -28,55 +70,132 @@ int main()
     staticFieldData.J = Vector3::Zero;
 
     StaticField staticField(staticFieldData);
-
-    for (int d = 1; d < 100; d++)
-    {    
-        // Initializing particle grid
-        ParticleGrid particleGrid(2, 2, 2000, 2000, Vector3(-1000, -1000), 0);
-
-        // Adding single particle to the grid
-        Particle testParticle(Constants::ElectronMass, Constants::ElectronCharge, Vector3(0, 0, 0), Vector3::Zero);
-        particleGrid.editParticlesInCell(0, 0).push_back(testParticle);
-
-        size_t NumInterations = 100 * d;
-        double timeStep = abs(Constants::ElectronMass * Constants::SpeedOfLight 
-            / (Constants::ElectronCharge * EZero * NumInterations));
-
-        // Initializing core
-        PicadorJrCore core(&staticField, &particleGrid, timeStep, NumInterations);
-
-        ParticleSolver particleSolver(&core);
-        core.insertModule(&particleSolver);
-
-        core.run();
-
-        // Checking results
-        auto& particles = core.getParticleGrid()->getParticlesInCell(0, 0);
-
-        double finalX = (sqrt(2) - 1) * Constants::ElectronMass * Constants::SpeedOfLight * Constants::SpeedOfLight / (Constants::ElectronCharge * EZero);
-        
-        double finalImpulseX = Constants::ElectronMass * Constants::SpeedOfLight;
-        double finalVelocityX = -1 * finalImpulseX / (Constants::ElectronMass * sqrt(1 + (finalImpulseX * finalImpulseX / ((Constants::ElectronMass * Constants::SpeedOfLight) 
-                        * (Constants::ElectronMass * Constants::SpeedOfLight)))));;
-
-        double xError = abs(finalX - particles[0].location.x);
-        double vError = abs(finalVelocityX - particles[0].velocity.x);
-
-        deltaTimes.push_back(timeStep);
-        locationErrors.push_back(xError);
-        velocityErrors.push_back(vError);
-    }
     
-    // Writing data into file
-    std::ofstream outputFile;
-    outputFile.open("./BorisData.txt", std::ios::out);
+    // Initializing particle grid
+    ParticleGrid particleGrid(2, 2, 2000, 2000, Vector3(-1000, -1000), 0);
+
+    // Adding single particle to the grid
+    Particle testParticle(Constants::ElectronMass, Constants::ElectronCharge, Vector3(0, 0, 0), Vector3::Zero);
+    particleGrid.editParticlesInCell(0, 0).push_back(testParticle);
+
+    size_t numInterations = iterations;
+    double timeStep = abs(Constants::ElectronMass * Constants::SpeedOfLight 
+        / (Constants::ElectronCharge * EZero * numInterations));
+
+    // Initializing core
+    PicadorJrCore core(&staticField, &particleGrid, timeStep, numInterations);
+
+    ParticleSolver particleSolver(&core);
+    core.insertModule(&particleSolver);
+
+    core.run();
+
+    // Checking results
+    auto& particles = core.getParticleGrid()->getParticlesInCell(0, 0);
+
+    double finalX = (sqrt(2) - 1) * Constants::ElectronMass * Constants::SpeedOfLight * Constants::SpeedOfLight / (Constants::ElectronCharge * EZero);
     
-    outputFile << "#DeltaTime, Location Error, Velocity Error" << std::endl << "/InverseX" << std::endl;
+    double finalImpulseX = Constants::ElectronMass * Constants::SpeedOfLight;
+    double finalVelocityX = -1 * finalImpulseX / (Constants::ElectronMass * sqrt(1 + (finalImpulseX * finalImpulseX / ((Constants::ElectronMass * Constants::SpeedOfLight) 
+                    * (Constants::ElectronMass * Constants::SpeedOfLight)))));;
 
-    for (size_t i = 0; i < deltaTimes.size(); i++)
-        outputFile << deltaTimes[i] << ", " << locationErrors[i] << ", " << velocityErrors[i] << std::endl;
+    outDeltaTimes.push_back(timeStep);
+    outLocationErrors.push_back((Vector3(finalX, 0, 0) - particles[0].location).size());
+    outVelocityErrors.push_back((Vector3(finalVelocityX, 0, 0) - particles[0].velocity).size());
+}
 
-    outputFile.close();
+void AccelerationTestY(std::vector<double>& outDeltaTimes, std::vector<double>& outLocationErrors, std::vector<double>& outVelocityErrors, size_t iterations)
+{
+    // Initializing static field
 
-    return 0;
+    double EZero = 1;
+
+    FieldData staticFieldData;
+    staticFieldData.E = Vector3(0, EZero, 0);
+    staticFieldData.B = Vector3::Zero;
+    staticFieldData.J = Vector3::Zero;
+
+    StaticField staticField(staticFieldData);
+    
+    // Initializing particle grid
+    ParticleGrid particleGrid(2, 2, 2000, 2000, Vector3(-1000, -1000), 0);
+
+    // Adding single particle to the grid
+    Particle testParticle(Constants::ElectronMass, Constants::ElectronCharge, Vector3(0, 0, 0), Vector3::Zero);
+    particleGrid.editParticlesInCell(0, 0).push_back(testParticle);
+
+    size_t numInterations = iterations;
+    double timeStep = abs(Constants::ElectronMass * Constants::SpeedOfLight 
+        / (Constants::ElectronCharge * EZero * numInterations));
+
+    // Initializing core
+    PicadorJrCore core(&staticField, &particleGrid, timeStep, numInterations);
+
+    ParticleSolver particleSolver(&core);
+    core.insertModule(&particleSolver);
+
+    core.run();
+
+    // Checking results
+    auto& particles = core.getParticleGrid()->getParticlesInCell(0, 0);
+
+    double finalX = (sqrt(2) - 1) * Constants::ElectronMass * Constants::SpeedOfLight * Constants::SpeedOfLight / (Constants::ElectronCharge * EZero);
+    
+    double finalImpulseX = Constants::ElectronMass * Constants::SpeedOfLight;
+    double finalVelocityX = -1 * finalImpulseX / (Constants::ElectronMass * sqrt(1 + (finalImpulseX * finalImpulseX / ((Constants::ElectronMass * Constants::SpeedOfLight) 
+                    * (Constants::ElectronMass * Constants::SpeedOfLight)))));;
+
+    outDeltaTimes.push_back(timeStep);
+    outLocationErrors.push_back((Vector3(0, finalX, 0) - particles[0].location).size());
+    outVelocityErrors.push_back((Vector3(0, finalVelocityX, 0) - particles[0].velocity).size());
+}
+
+
+void OscilationTest(std::vector<double>& outDeltaTimes, std::vector<double>& outLocationErrors, std::vector<double>& outVelocityErrors, size_t iterations)
+{
+    // Initializing static field
+
+    double BZero = 100;
+
+    double PZero = 1e-20;
+    double VZero = PZero / (Constants::ElectronMass * sqrt(1 + pow(PZero / (Constants::ElectronMass * Constants::SpeedOfLight), 2)));
+
+    FieldData staticFieldData;
+    staticFieldData.E = Vector3::Zero;
+    staticFieldData.B = Vector3(0, 0, BZero);
+    staticFieldData.J = Vector3::Zero;
+
+    StaticField staticField(staticFieldData);
+
+    // Initializing particle grid
+    ParticleGrid particleGrid(2, 2, 400, 400, Vector3(-200, -200), 0);
+
+    // Adding single particle to the grid
+    Particle testParticle(Constants::ElectronMass, Constants::ElectronCharge, Vector3::Zero, Vector3(VZero, 0, 0));
+    particleGrid.editParticlesInCell(0, 0).push_back(testParticle);
+
+    size_t numInterations = iterations;
+    double timeStep = Constants::PI * Constants::ElectronMass * Constants::SpeedOfLight / (abs(Constants::ElectronCharge) * BZero * numInterations) 
+                        * sqrt(1 + pow(PZero / (Constants::ElectronMass * Constants::SpeedOfLight), 2));
+
+    // Initializing core
+    PicadorJrCore core(&staticField, &particleGrid, timeStep, numInterations);
+
+    ParticleSolver particleSolver(&core);
+    core.insertModule(&particleSolver);
+
+    core.run();
+
+    // Checking results
+    auto& particles = core.getParticleGrid()->getParticlesInCell(0, 0);
+
+    double finalY = -2 * PZero * Constants::SpeedOfLight / (Constants::ElectronCharge * BZero);
+    
+    double finalImpulseX = -PZero;
+    double finalVelocityX = finalImpulseX / (Constants::ElectronMass * sqrt(1 + (finalImpulseX * finalImpulseX / ((Constants::ElectronMass * Constants::SpeedOfLight) 
+                    * (Constants::ElectronMass * Constants::SpeedOfLight)))));
+
+    outDeltaTimes.push_back(timeStep);
+    outLocationErrors.push_back((Vector3(0, finalY, 0) - particles[0].location).size());
+    outVelocityErrors.push_back((Vector3(finalVelocityX, 0, 0) - particles[0].velocity).size());
 }
