a4424d77d6dba37973e107293d424f30cf2c018f
K <Akarfire@gmail.com>
2025-10-30 21:39:37 +0300

    [TEST] Particle Solver Oscillation test

--- PICADOR-Jr-Project/test/test_particle_solver.cpp
+++ PICADOR-Jr-Project/test/test_particle_solver.cpp
@@ -58,3 +58,61 @@ TEST(ParticleSolver, RelativisticAccelerationInStaticField)
     EXPECT_DOUBLE_EQ(0, particles[0].velocity.y);
     EXPECT_DOUBLE_EQ(0, particles[0].velocity.z);
 }
+
+
+TEST(ParticleSolver, OscilationInStaticField)
+{
+    // Initializing static field
+
+    double BZero = 100;
+
+    double PZero = 1e-20;
+    double VZero = PZero / Constants::ElectronMass / sqrt(1 + pow(PZero / (Constants::ElectronMass * Constants::SpeedOfLight), 2));
+
+    FieldData staticFieldData;
+    staticFieldData.E = Vector3::Zero;
+    staticFieldData.B = Vector3(0, 0, BZero);
+    staticFieldData.J = Vector3::Zero;
+
+    StaticField staticField(staticFieldData);
+
+    // Initializing particle grid
+    ParticleGrid particleGrid(2, 2, 400, 400, Vector3(-200, -200), 0);
+
+    // Adding single particle to the grid
+    Particle testParticle(Constants::ElectronMass, Constants::ElectronCharge, Vector3::Zero, Vector3(VZero, 0, 0));
+    particleGrid.editParticlesInCell(0, 0).push_back(testParticle);
+
+    size_t NumInterations = 100;
+    double timeStep = Constants::PI * Constants::ElectronMass * Constants::SpeedOfLight / (abs(Constants::ElectronCharge) * BZero * NumInterations) 
+                        * sqrt(1 + pow(PZero / (Constants::ElectronMass * Constants::SpeedOfLight), 2));
+
+    // Initializing core
+    PicadorJrCore core(&staticField, &particleGrid, timeStep, NumInterations);
+
+    ParticleSolver particleSolver(&core);
+    core.insertModule(&particleSolver);
+
+    core.run();
+
+    // Checking results
+    auto& particles = core.getParticleGrid()->getParticlesInCell(0, 0);
+
+    ASSERT_EQ(1, particles.size());
+
+    double finalY = -2 * PZero * Constants::SpeedOfLight / (Constants::ElectronCharge * BZero);
+    
+    double finalImpulseX = -PZero;
+    double finalVelocityX = finalImpulseX / (Constants::ElectronMass * sqrt(1 + (finalImpulseX * finalImpulseX / ((Constants::ElectronMass * Constants::SpeedOfLight) 
+                    * (Constants::ElectronMass * Constants::SpeedOfLight)))));
+
+    EXPECT_DOUBLE_EQ(0, particles[0].location.x);
+    EXPECT_DOUBLE_EQ(finalY, particles[0].location.y);
+    EXPECT_DOUBLE_EQ(0, particles[0].location.z);
+
+    //EXPECT_NEAR()
+
+    EXPECT_DOUBLE_EQ(finalVelocityX, particles[0].velocity.x);
+    EXPECT_DOUBLE_EQ(0, particles[0].velocity.y);
+    EXPECT_DOUBLE_EQ(0, particles[0].velocity.z);
+}
