e673ebe37b427494e3e44b4d86002ffb9291f9c9
vassyusha <romanoffmiss@yandex.ru>
2025-10-31 13:01:40 +0300

    module current depositore is done

--- PICADOR-Jr-Project/Include/Module_CurrentDepositor.h
+++ PICADOR-Jr-Project/Include/Module_CurrentDepositor.h
@@ -7,30 +7,20 @@
 class CurrentDepositor : public Module
 {
     FieldGrid* fieldGrid;
+    ParticleGrid* particleGrid;
 
 public:
     CurrentDepositor(PicadorJrCore* core_): Module(core_) {}
 
-    virtual ModuleExecutionStatus onBegin() override
-    {
-        // Casting core's field container to FieldGrid
-        fieldGrid = dynamic_cast<FieldGrid*>(core->getFieldContainer());
+    // Called by the core before entering the simulation loop
+    virtual ModuleExecutionStatus onBegin() override;
 
-        return ModuleExecutionStatus::Success;
-    }
+    // Called on every iteration of the simulation loop
+    virtual ModuleExecutionStatus onUpdate() override;
 
-    virtual ModuleExecutionStatus onUpdate() override
-    {
-        if (fieldGrid)
-        {
-            return ModuleExecutionStatus::Success;
-        }
+    // Called after the core has exited the simulation loop
+    virtual ModuleExecutionStatus onEnd() override { return ModuleExecutionStatus::Success; }
 
-        return ModuleExecutionStatus::Error;
-    }
-
-    virtual ModuleExecutionStatus onEnd() override
-    {
-        return ModuleExecutionStatus::Success;
-    }
+    //deposition of particles in one cell
+    void deposite(int i, int j);
 };
