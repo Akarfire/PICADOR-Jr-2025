88f7759a1b9719835cdeb96d096bfef17558d065
vassyusha <romanoffmiss@yandex.ru>
2025-11-07 13:35:12 +0300

    some changes with constants

---
+++ PICADOR-Jr-Project/Source/Constants.cpp
@@ -0,0 +1,6 @@
+#include "Constants.h"
+
+const double Constants::SpeedOfLight = 29979245800;
+const double Constants::ElectronMass = 9.10938215e-28;
+const double Constants::ElectronCharge = -4.80320427e-10;
+const double Constants::PI = 3.14159265359;
