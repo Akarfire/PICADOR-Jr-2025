18200079f469663d2a7073914bf599f8f760edd4
K <Akarfire@gmail.com>
2025-11-07 13:46:11 +0300

    [DEV] Alternative particle constructor with type

--- PICADOR-Jr-Project/Include/Particle.h
+++ PICADOR-Jr-Project/Include/Particle.h
@@ -20,6 +20,7 @@ struct Particle
 	unsigned short int trackingID = 0;
 
 	Particle(double inMass, double inCharge, const Vector3& initialLocation, const Vector3 initialImpulse): mass(inMass), charge(inCharge), location(initialLocation), impulse(initialImpulse) {}
+	Particle(const ParticleType& inType, double inMass, double inCharge, const Vector3& initialLocation = Vector3::Zero, const Vector3 initialImpulse = Vector3::Zero): type(inType), mass(inMass), charge(inCharge), location(initialLocation), impulse(initialImpulse) {}
 
 	// Calculate and return particle velocity based on its impulse and mass
 	Vector3 getVelocity() const
