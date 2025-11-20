653be156cfb479462bf28dd5dba65880d3ca0dae
K <Akarfire@gmail.com>
2025-11-20 18:21:58 +0300

    [DEV] Particle weight & comments

--- PICADOR-Jr-Project/Include/Particle.h
+++ PICADOR-Jr-Project/Include/Particle.h
@@ -7,20 +7,33 @@ enum class ParticleType {Photon, Proton, Electron, Positron};
 
 struct Particle
 {
+	// Type of the particle (for complex interactions)
 	ParticleType type;
 
-	double mass, charge;
+	// Number of physical particles represented by this 'virtual' particle
+	size_t weight;
 
+	// Mass of a single particle
+	double mass;
+	// Charge of a single particle
+	double charge;
+
+	// Current location in world space
 	Vector3 location;
+	// Current impulse in world space
 	Vector3 impulse;
 
+	// Special flag, used for transferning particles between ParticleGrid cells
 	bool transferFlag;
 	
 	// Optional tracking ID for particle identification during visualization or data sampling
 	unsigned short int trackingID = 0;
 
-	Particle(double inMass, double inCharge, const Vector3& initialLocation, const Vector3 initialImpulse): mass(inMass), charge(inCharge), location(initialLocation), impulse(initialImpulse) {}
-	Particle(const ParticleType& inType, double inMass, double inCharge, const Vector3& initialLocation = Vector3::Zero, const Vector3 initialImpulse = Vector3::Zero): type(inType), mass(inMass), charge(inCharge), location(initialLocation), impulse(initialImpulse) {}
+	Particle(double mass_, double charge_, const Vector3& initialLocation, const Vector3 initialImpulse, size_t weight_ = 1): 
+		mass(mass_), charge(charge_), location(initialLocation), impulse(initialImpulse), weight(weight_) {}
+
+	Particle(const ParticleType& type_, double mass_, double charge_, size_t weight_ = 1, const Vector3& initialLocation = Vector3::Zero, const Vector3 initialImpulse = Vector3::Zero): 
+		type(type_), mass(mass_), charge(charge_), location(initialLocation), impulse(initialImpulse), weight(weight_) {}
 
 	// Calculate and return particle velocity based on its impulse and mass
 	Vector3 getVelocity() const;
